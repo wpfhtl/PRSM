a03a4e353483a7728c8ef48a8592a53fb9b69ba2
HP_W10 <wpfhtl@gmail.com>
2017-11-24 14:00:31 +0800

    main function

---
+++ solvePWRSMulti/main.m
@@ -0,0 +1,4 @@
+% example: pic 138
+run_pwrs_red( [138], './2Frames/', 10, 0.4, 0.045, 1.0, 20, 20, 25 );
+% example: set: p.saveProposals =true; p.use3Frames = true;p.usePrevProps = true;
+run_pwrs_red( [151,27], './3Frames/', [8,9,10], 0.4, 0.045, 1.0, 20, 20, 25 );
